f8c26b22308619d23110e3c785605ac6feb98aae
TsujiEisho <s1911245@s.tsukuba.ac.jp>
2021-03-12 16:34:44 +0900

    euclid 完成版

--- algo/euclid.cpp
+++ algo/euclid.cpp
@@ -3,6 +3,24 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define _GLIBCXX_DEBUG
 
+
+// ユークリッドの互除法。
+// 2整数mとnを受け取ってその最大公約数を返す。
+
+int euclid(int m , int n){
+    int r = m%n;
+    if (r==0){
+        return n;
+    }
+    else
+        m = n; n =r;
+        return euclid(m, n);
+    }
+}
+
+
 int main() {
-    cout << "Hello" << endl;
+    int m, n;
+    cin >> m >> n;
+    cout << euclid(m, n) << endl;
 }
